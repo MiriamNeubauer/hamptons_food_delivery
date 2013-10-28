48f5436f010facbe09e09e04ac0f5ebd70b82e09
MiriamNeubauer <miriam.neubauer@cdtm.de>
2013-10-29 00:59:18 +0100

    memem mailgun fuckup tryna fix it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,18 @@ HamptonsfooddeliveryApp::Application.configure do
 
 
 #EMAIL comment out
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-     :authentication => :plain,
-     :address => "smtp.mailgun.org",
-     :port => 587,
-     :domain => "hamptonsfooddelivery.herokuapp.com",
-     :user_name => "postmaster@hamptonsfooddelivery.com",
-     :password => "0uin49fqrim4"
-     # :enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.smtp_settings = {
+  #    :authentication => :plain,
+  #    :address => "smtp.mailgun.org",
+  #    :port => 587,
+  #    :domain => "hamptonsfooddelivery.herokuapp.com",
+  #    :user_name => "postmaster@hamptonsfooddelivery.com",
+  #    :password => "0uin49fqrim4"
+  #    # :enable_starttls_auto: true
+  # }
 #-----------
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
