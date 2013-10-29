434c17087219871f62ee28a2b0fb59818627f208
MiriamNeubauer <miriam.neubauer@cdtm.de>
2013-10-29 01:28:51 +0100

    memem mailgun fuckup tryna fix it

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
      #    format.html { render action: 'new' }
      #    format.json { render json: @user.errors, status: :unprocessable_entity }
     	# end #end for if-else
-  	end #end for do
+  	#end for do
 
   	redirect_to order #das gehtbzu der order variable die wir gerade kreiert ahben
 		#am ende von create IMMER REDIRECT
